c01f875dc0c7decf43b8a2e90fa4a88233f10f6a
Alex <lilith.luthien@gmail.com>
2017-10-16 12:42:57 -0300

    Transformador simple

---
+++ SlackifyApp.Tests/Model/SimpleTransformatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SlackifyApp;
+using SlackifyApp.Model;
+
+namespace SlackifyApp.Tests.Model
+{
+    [TestClass]
+    public class SimpleTransformatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SimpleTransformator transformator = new SimpleTransformator();
+
+            String result = transformator.transform("sarlanga");
+
+            Assert.AreEqual("sarlanga", result);
+        }
+    }
+}
---
+++ SlackifyApp/Models/SimpleTransformator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SlackifyApp.Model
+{
+    public class SimpleTransformator
+    {
+        public SimpleTransformator()
+        {
+        }
+
+        public String transform(string input)
+        {
+            return input;
+        }
+    }
+}
