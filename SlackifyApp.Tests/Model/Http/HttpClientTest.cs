914fd53ca34d13f372f37ef782bfcd3cb4450ab2
Alex <lilith.luthien@gmail.com>
2017-10-16 16:37:43 -0300

    HTTP client made easy
    No await shit

---
+++ SlackifyApp.Tests/Model/Http/HttpClientTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+
+namespace SlackifyApp.Tests.Model.Http
+{
+    [TestClass]
+    public class HttpClientTest
+    {
+        [TestMethod]
+        public void http_client_can_get_stuff()
+        {
+            SimpleHttpClient client = new SimpleHttpClient
+            {
+                BaseAddress = new Uri("http://table-flip.herokuapp.com/")
+            };
+
+            string response = client.Get("hercules");
+
+            Assert.AreEqual("(/ .□.) ︵╰(゜Д゜)╯︵ /(.□. )", response);
+        }
+
+        [TestMethod]
+        public void http_client_can_get_stuff_without_base_url()
+        {
+            SimpleHttpClient client = new SimpleHttpClient();
+
+            string response = client.Get("http://table-flip.herokuapp.com/hercules");
+
+            Assert.AreEqual("(/ .□.) ︵╰(゜Д゜)╯︵ /(.□. )", response);
+        }
+    }
+}
---
+++ SlackifyApp/Models/Http/SimpleHttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace SlackifyApp.Tests.Model.Http
+{
+    public class SimpleHttpClient : HttpClient
+    {
+        public string Get(string url)
+        {
+            HttpResponseMessage response = this.GetAsync(BaseAddress + url).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
