00d718d56bb3ee824c2f262c27aa05485f0c8010
Alex <lilith.luthien@gmail.com>
2017-12-09 18:13:52 -0300

    Busqueda por endpoint
    http://localhost:50748/endpoint/cuddly-numberless-table

--- SlackifyApp/Controllers/EndpointController.cs
+++ SlackifyApp/Controllers/EndpointController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SlackifyApp.Models;
 
 namespace SlackifyApp.Controllers
 {
     public class EndpointController : Controller
     {
+
+        private ConfigureDBContext _db = new ConfigureDBContext();
         // GET: Endpoint
         public ActionResult Process(string endpoint)
         {
-            Console.WriteLine("Sarlanga");
-            return null;
+            DataBaseConfigure dataBaseConfigure = _db.DB.First(b => b.endpoint == endpoint);
+            return Json(dataBaseConfigure, JsonRequestBehavior.AllowGet);
         }
     }
 }
